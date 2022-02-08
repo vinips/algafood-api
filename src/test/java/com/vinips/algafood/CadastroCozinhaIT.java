29ff5509029cef080f3d63de2e83b94400b716fa
Vinícius Pedro da Silveira <viniciusps_ph@hotmail.com>
2022-02-08 19:01:22 -0300

    Finalizando aula 10.13

--- src/test/java/com/vinips/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/vinips/algafood/CadastroCozinhaIT.java
@@ -1,11 +1,15 @@
 package com.vinips.algafood;
 
+import org.flywaydb.core.Flyway;
 import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import io.restassured.RestAssured;
@@ -14,41 +18,50 @@ import io.restassured.http.ContentType;
 @RunWith(SpringRunner.class)
 //Esse webEnvironment serve para o TomCat ser levantado e definimos a porta na variável port
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//Serve para indicar qual arquivo .properties quer usar, se não usar a anotação ele usa a padrão.
+@TestPropertySource("/application-test.properties")
 public class CadastroCozinhaIT {
-								
+
 	@LocalServerPort
 	private int port;
 
-	@Test
-	public void testarResultado200ConsultaCozinha() {
-		RestAssured.given()
-			.basePath("/cozinhas")
-			.port(port)
-			.accept(ContentType.JSON)
-		.when()
-			.get()
-		.then()
-			.statusCode(HttpStatus.OK.value());
-	}
-	
-	@Test
-	public void testarConsultaCozinhaRetorna9Cozinhas() {
-		//RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-		
-		RestAssured.given()
-			.basePath("/cozinhas")
-			.port(port)
-			.accept(ContentType.JSON)
-		.when()
-			.get()
-		.then()
-			.body("", Matchers.hasSize(9));
-			//.body("nome", Matchers.hasItems("Italiana", "Francesa"));
+	@Autowired
+	private Flyway flyway;
+
+	// Essa anotação Before faz com que esse método seja executado sempre antes de
+	// cada método anotado com @Test rodar
+	@Before
+	public void setUp() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		RestAssured.port = port;
+		RestAssured.basePath = "/cozinhas";
+
+		// Serve para dar um callback do flyway e antes de cada método @Test executar,
+		// vai chamar o arquivo afterMigrate.sql e resetando o banco para o estado
+		// original. Só funciona se o application.properties que a classe test ta usando
+		// estiver com o callback configurado.
+		flyway.migrate();
 	}
 
-	
-	//TESTES DE INTEGRAÇÃO
-	
+	@Test
+	public void deveRetornarStatus200_QuandoConsultarCozinhas() {
+		RestAssured.given().accept(ContentType.JSON).when().get().then().statusCode(HttpStatus.OK.value());
+	}
+
+	@Test
+	public void deveConter4Cozinhas_QuandoConsultarCozinhas() {
+		RestAssured.given().accept(ContentType.JSON).when().get().then().body("", Matchers.hasSize(9));
+	}
+
+	@Test
+	public void testRetornarStatus201_QuandoCadastrarCozinha() {
+		RestAssured.given().body("{ \"nome\": \"Chinesa\" }").contentType(ContentType.JSON).accept(ContentType.JSON)
+				.when().post().then().statusCode(HttpStatus.CREATED.value());
+
+	}
+
+	// TESTES DE INTEGRAÇÃO
+
 //	@Autowired
 //	private CadastroCozinhaService zcadastroCozinhaService;
 //
