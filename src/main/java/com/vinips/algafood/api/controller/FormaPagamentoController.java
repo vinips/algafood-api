721e212b04e293854028dee4870628f1e0ea6cfc
Vinícius Pedro da Silveira <viniciusps_ph@hotmail.com>
2022-03-02 18:34:25 -0300

    Finalizando criação do CRUD e API do Forma de pagamento. Aula 12.5

--- src/main/java/com/vinips/algafood/api/controller/FormaPagamentoController.java
+++ src/main/java/com/vinips/algafood/api/controller/FormaPagamentoController.java
@@ -1,10 +1,87 @@
 package com.vinips.algafood.api.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.vinips.algafood.api.model.assembler.FormaPagamentoDTOAssembler;
+import com.vinips.algafood.api.model.disassembler.FormaPagamentoInputDisassembler;
+import com.vinips.algafood.api.model.dto.FormaPagamentoDTO;
+import com.vinips.algafood.api.model.input.FormaPagamentoInput;
+import com.vinips.algafood.domain.model.FormaPagamento;
+import com.vinips.algafood.domain.repository.FormaPagamentoRepository;
+import com.vinips.algafood.domain.service.CadastroFormaPagamentoService;
+
 @RestController
-@RequestMapping("/formas-Pagamento")
+@RequestMapping("/formas-pagamento")
 public class FormaPagamentoController {
+
+	@Autowired
+	private FormaPagamentoRepository formaPagamentoRepository;
+
+	@Autowired
+	private CadastroFormaPagamentoService cadastroFormaPagamento;
+
+	@Autowired
+	private FormaPagamentoDTOAssembler formaPagamentoAssembler;
+
+	@Autowired
+	private FormaPagamentoInputDisassembler formaPagamentoDisassembler;
+
+	@GetMapping
+	public ResponseEntity<List<FormaPagamentoDTO>> listar() {
+		List<FormaPagamento> formasPagamento = formaPagamentoRepository.findAll();
+
+		if (formasPagamento != null && !formasPagamento.isEmpty()) {
+			return ResponseEntity.ok(formaPagamentoAssembler.toCollectionDTO(formasPagamento));
+		}
+
+		return ResponseEntity.noContent().build();
+
+	}
+
+	@GetMapping("/{formaPagamentoId}")
+	public FormaPagamentoDTO buscar(@PathVariable Long formaPagamentoId) {
+		FormaPagamento formaPagamento = cadastroFormaPagamento.buscarOuFalhar(formaPagamentoId);
+
+		return formaPagamentoAssembler.toDTO(formaPagamento);
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public FormaPagamentoDTO adicionar(@Valid @RequestBody FormaPagamentoInput formaPagamentoInput) {
+		FormaPagamento formaPagamento = formaPagamentoDisassembler.toDomainModel(formaPagamentoInput);
+
+		return formaPagamentoAssembler.toDTO(cadastroFormaPagamento.salvar(formaPagamento));
+	}
+
+	@PutMapping("/{formaPagamentoId}")
+	public FormaPagamentoDTO atualizar(@PathVariable Long formaPagamentoId,
+			@Valid @RequestBody FormaPagamentoInput formaPagamentoInput) {
+		FormaPagamento formaPagamentoAtual = cadastroFormaPagamento.buscarOuFalhar(formaPagamentoId);
+
+		formaPagamentoDisassembler.copyToDomainObject(formaPagamentoInput, formaPagamentoAtual);
+
+		return formaPagamentoAssembler.toDTO(cadastroFormaPagamento.salvar(formaPagamentoAtual));
+	}
+
+	@DeleteMapping("/{formaPagamentoId}")
+	@ResponseStatus(value = HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long formaPagamentoId) {
+		cadastroFormaPagamento.excluir(formaPagamentoId);
+	}
 
 }
--- src/main/java/com/vinips/algafood/domain/service/CadastroFormaPagamentoService.java
+++ src/main/java/com/vinips/algafood/domain/service/CadastroFormaPagamentoService.java
@@ -7,79 +7,46 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.vinips.algafood.domain.exception.EntidadeEmUsoException;
-import com.vinips.algafood.domain.exception.RestauranteNaoEncontradoException;
-import com.vinips.algafood.domain.model.Cozinha;
-import com.vinips.algafood.domain.model.Restaurante;
-import com.vinips.algafood.domain.repository.RestauranteRepository;
+import com.vinips.algafood.domain.exception.FormaPagamentoNaoEncontradaException;
+import com.vinips.algafood.domain.model.FormaPagamento;
+import com.vinips.algafood.domain.repository.FormaPagamentoRepository;
 
 @Service
 public class CadastroFormaPagamentoService {
 	
-	private static final String MSG_RESTAURANTE_EM_USO = "Restaurante de código %d não pode ser removida pois está em uso";
+	private static final String MSG_FORMA_PAGAMENTO_EM_USO = "Forma de Pagamento de código %d não pode ser removida pois está em uso";
 
 	@Autowired
-	private RestauranteRepository restauranteRepository;
-
-	@Autowired
-	private CadastroCozinhaService cadastroCozinha;
+	private FormaPagamentoRepository formaPagamentoRepository;
 
 	//Boa prática anotar os métodos que fazem manipulação de dados com o @Transactional para não ocorrer erros
 	@Transactional
-	public Restaurante salvar(Restaurante restaurante) {
-		Long cozinhaId = restaurante.getCozinha().getId();
-		Cozinha cozinha = cadastroCozinha.buscarOuFalhar(cozinhaId);
-
-		restaurante.setCozinha(cozinha);
-		return restauranteRepository.save(restaurante);
+	public FormaPagamento salvar(FormaPagamento formaPagamento) {
+		return formaPagamentoRepository.save(formaPagamento);
 	}
 	
-	//Aqui ele vai retornar um Restaurante
-	//Caso não encontre a restaurante, vai lançar uma Exception.
-	public Restaurante buscarOuFalhar(Long restauranteId) {
-		return restauranteRepository.findById(restauranteId).orElseThrow(
-				() -> new RestauranteNaoEncontradoException(restauranteId));
+	//Aqui ele vai retornar uma Forma de Pagamento
+	//Caso não encontre a Forma de Pagamento, vai lançar uma Exception.
+	public FormaPagamento buscarOuFalhar(Long formaPagamentoId) {
+		return formaPagamentoRepository.findById(formaPagamentoId).orElseThrow(
+				() -> new FormaPagamentoNaoEncontradaException(formaPagamentoId));
 	}
 
 	@Transactional
-	public void excluir(Long restauranteId) {
+	public void excluir(Long formaPagamentoId) {
 		try {
-			restauranteRepository.deleteById(restauranteId);
+			formaPagamentoRepository.deleteById(formaPagamentoId);
 			
 			//Estamos forçando o JPA a fazer o flush da transação para que se de erro, ele caia na nossa Exception.
 			//Caso contrário ele vai dar o flush só quando a transação fechar, que nesse caso é no fim do nosso método excluir.
-			restauranteRepository.flush();
+			formaPagamentoRepository.flush();
 		} catch (EmptyResultDataAccessException e) {
-			throw new RestauranteNaoEncontradoException(restauranteId);
+			throw new FormaPagamentoNaoEncontradaException(formaPagamentoId);
 		} catch (DataIntegrityViolationException e) {
 			throw new EntidadeEmUsoException(
-					String.format(MSG_RESTAURANTE_EM_USO, restauranteId));
+					String.format(MSG_FORMA_PAGAMENTO_EM_USO, formaPagamentoId));
 		}
 
-	}
-	
-	@Transactional
-	public void ativar(Long restauranteId) {
-		Restaurante restaurante = buscarOuFalhar(restauranteId);
-		
-		restaurante.ativar();
-		
-		//Não precisamos chamar o salvar nesse caso pois essa instância de restaurante que
-		//o buscarOuFalhar nos devolveu esta sendo gerenciado pelo contexto de persistência do JPA.
-		//Então qualquer modificação nele vai ser persistido no banco de dados quando 
-		//o Transactional chegar no final e dar o merge automático.
-	}
-	
-
-	@Transactional
-	public void inativar(Long restauranteId) {
-		Restaurante restaurante = buscarOuFalhar(restauranteId);
-		
-		restaurante.inativar();
-		
-		//Não precisamos chamar o salvar nesse caso pois essa instância de restaurante que
-		//o buscarOuFalhar nos devolveu esta sendo gerenciado pelo contexto de persistência do JPA.
-		//Então qualquer modificação nele vai ser persistido no banco de dados quando 
-		//o Transactional chegar no final e dar o merge automático.
 	}
 
 }
