333c5b6fdc6c2936ce6ed1b29f3b2c23be749649
Vinícius Pedro da Silveira <viniciusps_ph@hotmail.com>
2022-05-07 16:14:25 -0300

    Aula

---
+++ src/main/java/com/vinips/algafood/domain/model/FotoProduto.java
@@ -0,0 +1,111 @@
+package com.vinips.algafood.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+
+@Entity
+public class FotoProduto {
+	
+	@Id
+	@Column(name = "produto_id")
+	private Long id;
+	
+	@OneToOne(fetch = FetchType.LAZY)
+	@MapsId
+	private Produto produto;
+	
+	@Column(name = "nome_arquivo")
+	private String nomeArquivo;
+	
+	private String descricao;
+	
+	@Column(name = "content_type")
+	private String contentType;
+	
+	private Long tamanho;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+	public String getNomeArquivo() {
+		return nomeArquivo;
+	}
+
+	public void setNomeArquivo(String nomeArquivo) {
+		this.nomeArquivo = nomeArquivo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getContentType() {
+		return contentType;
+	}
+
+	public void setContentType(String contentType) {
+		this.contentType = contentType;
+	}
+
+	public Long getTamanho() {
+		return tamanho;
+	}
+
+	public void setTamanho(Long tamanho) {
+		this.tamanho = tamanho;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FotoProduto other = (FotoProduto) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "FotoProduto [id=" + id + ", produto=" + produto + ", nomeArquivo=" + nomeArquivo + ", descricao="
+				+ descricao + ", contentType=" + contentType + ", tamanho=" + tamanho + "]";
+	}
+	
+
+}
