0e1cff68d9884d4a69c9d8e10073e9206765dcb2
Vinícius Pedro da Silveira <viniciusps_ph@hotmail.com>
2022-05-23 22:11:57 -0300

    Refatorando aula 14.9

--- src/main/java/com/vinips/algafood/infrastructure/service/email/SandboxEnvioEmailService.java
+++ src/main/java/com/vinips/algafood/infrastructure/service/email/SandboxEnvioEmailService.java
@@ -1,40 +1,25 @@
 package com.vinips.algafood.infrastructure.service.email;
 
+import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 
 import com.vinips.algafood.core.email.EmailProperties;
-import com.vinips.algafood.infrastructure.exception.EmailException;
 
 public class SandboxEnvioEmailService extends SmtpEnvioEmailService{
 
 	@Autowired
-	private JavaMailSender mailSender;
-	
-	@Autowired
 	private EmailProperties emailProperties;
 	
-	@Override
-	public void enviar(Mensagem mensagem) {
-		try {
-			String corpo = processarTemplate(mensagem);
-			
-			MimeMessage mimeMessage = mailSender.createMimeMessage();
-			
-			MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "UTF-8");
-			helper.setFrom(emailProperties.getRemetente());
-			helper.setTo(emailProperties.getSandbox().getDestinatario());
-			helper.setSubject(mensagem.getAssunto());
-			//o true é que vamos enviar no padrão HTML e não no padrão texto
-			helper.setText(corpo, true);
-			
-			mailSender.send(mimeMessage);
-		} catch (Exception e) {
-			throw new EmailException("Não foi possível enviar e-mail", e);
-		}
+	protected MimeMessage criarMimeMessage(Mensagem mensagem) throws MessagingException {
+		MimeMessage mimeMessage = super.criarMimeMessage(mensagem);
+		
+		MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "UTF-8");
+		helper.setTo(emailProperties.getSandbox().getDestinatario());
+	
+		return mimeMessage;
 	}
 	
 }
