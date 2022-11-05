97c55bd7bdbbfd136e3f33e3f3cfb9b628d195e4
Vinícius Pedro da Silveira <viniciusps_ph@hotmail.com>
2022-11-05 00:50:33 -0300

    Finalizando aula 20.19 e Módulo 20 de Versionamento de API

--- src/main/java/com/vinips/algafood/core/web/ApiDeprecationHandler.java
+++ src/main/java/com/vinips/algafood/core/web/ApiDeprecationHandler.java
@@ -3,9 +3,11 @@ package com.vinips.algafood.core.web;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+//VERSÕES MAIS NOVAS UTILIZAM HandlerInterceptor POIS HandlerInterceptorAdapter FOI DEPRECIADA
 //Essa classe serve para interceptar métodos dos controladores
 @Component
 public class ApiDeprecationHandler extends HandlerInterceptorAdapter {
@@ -15,7 +17,12 @@ public class ApiDeprecationHandler extends HandlerInterceptorAdapter {
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		if (request.getRequestURI().startsWith("/v2/")) {
-			response.addHeader("X-AlgaFood-Deprecated", "Essa versão da API foi feita para fins de estudo e não deve ser utilizada. Utilize a versão 1");
+			//Para desligar a versão da API
+			response.setStatus(HttpStatus.GONE.value());
+			return false;
+			
+			//Para informar que esta depreciada
+			//response.addHeader("X-AlgaFood-Deprecated", "Essa versão da API foi feita para fins de estudo e não deve ser utilizada. Utilize a versão 1");
 		}
 		
 		return true;
