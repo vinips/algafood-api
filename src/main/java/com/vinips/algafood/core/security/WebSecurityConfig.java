03194769fab91a071b4ce6b006e1c6e7030bd0c4
Vinícius Pedro da Silveira <viniciusps_ph@hotmail.com>
2023-04-13 21:46:58 -0300

    Finalizando aula 22.4

--- src/main/java/com/vinips/algafood/core/security/WebSecurityConfig.java
+++ src/main/java/com/vinips/algafood/core/security/WebSecurityConfig.java
@@ -1,19 +1,23 @@
 package com.vinips.algafood.core.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	//Aula 22.3
+	//Trocamos a configuração do Security para fazer a authenticação apenas pelo HttpBasic, sem formulário de login nem nada.
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		//Aula 22.3
-		//Trocamos a configuração do Security para fazer a authenticação apenas pelo HttpBasic, sem formulário de login nem nada.
 		http.httpBasic()
 			.and()
 			.authorizeRequests()
@@ -28,6 +32,26 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.and()
 				//O Csrf pode ser desabilitado nesse caso pois ele é uma proteção quando usamos cookie. Como desabilitamos os cookies e vamos usar autenticação em cada requisioção, ele pode ser desabilitado.
 				.csrf().disable();
+	}
+	
+	//Aula 22.4
+	//Criamos uma lista de usuários em memória.
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("thiago")
+				.password(passwordEncoder().encode("123"))
+				.roles("ADMIN")
+			.and()
+				.withUser("joao")
+				.password(passwordEncoder().encode("123"))
+				.roles("ADMIN");
+	}
+	
+	//Método para encriptografar senhas
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 	
 }
